377fe6f89be52f73afa9371a0f31e92c3770ac8c
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-13 13:31:23 +0100

    Correction1

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -1,13 +1,1 @@
-#include <stdio.h>
 
-int main(int argc, char const *argv[])
-{
-    int a , b;
-
-    scanf("%d %d", &a, &b);
-    a = a + b;
-    b = a - b;
-    a = a - b;
-    printf("Après échange: a = %d, b = %d", a, b);
-    return 0;
-}
