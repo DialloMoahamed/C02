34907790ae53db3f72c696ae212c1adc321f0139
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-13 01:13:25 +0100

    Push exo2

--- sum_two_integers.c
+++ sum_two_integers.c
@@ -3,7 +3,6 @@
 int main(int argc, char const *argv[])
 {
     int a, b, sum;
-    printf("Saisir deux entiers : ");
     scanf("%d %d", &a, &b);
     sum = a + b;
     printf("La somme est : %d", sum);
