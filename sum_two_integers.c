40672948759e52b6a9a5b84c3be8d95c682c2602
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-13 01:05:28 +0100

    Correction exo2

--- sum_two_integers.c
+++ sum_two_integers.c
@@ -3,10 +3,8 @@
 int main(int argc, char const *argv[])
 {
     int a, b, sum;
-    printf("Saisir le premier entier : ");
-    scanf("%d", &a);
-    printf("Saisir le deuxième entier : ");
-    scanf("%d", &b);
+    printf("Saisir deux entiers : ");
+    scanf("%d %d", &a, &b);
     sum = a + b;
     printf("La somme est : %d", sum);
     return 0;
