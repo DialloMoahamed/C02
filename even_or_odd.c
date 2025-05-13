9aa9e3f29ee779f7965af9cb38fbfbffa1d93f37
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-13 13:42:29 +0100

    Correction3

--- even_or_odd.c
+++ even_or_odd.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
+int main()
 {
     int a;
 
     scanf("%d", &a);
 
     if (a % 2 == 0)
-        printf("%d est un nombre pair.", a);
+        printf("%d est un nombre pair.\n", a);
     else
-        printf("%d est un nombre impair.", a);
+        printf("%d est un nombre impair.\n", a);
 
     return 0;
 }
