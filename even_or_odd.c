728a4226e6b62bbd41cc4f050be665c562f8553a
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-13 11:58:26 +0100

    Correction2 de lexo3

--- even_or_odd.c
+++ even_or_odd.c
@@ -10,7 +10,7 @@ int main(int argc, char const *argv[])
     {
         printf("\n%d est un nombre pair.", a);
     }
-    else if (a % 2 != 0)
+    else
     {
         printf("\n%d est un nombre impair.", a);
     }
