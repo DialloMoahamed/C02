321fc6ac6640b92af5d989d306b331af136e187f
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-13 23:42:40 +0100

    Commit

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -3,13 +3,12 @@
 int main()
 {
   int a, b;
-  printf("Entrer deux nombres :");
   scanf("%d %d", &a, &b);
 
   a = a + b;
   b = a - b;
   a = a - b;
-  printf("Après échange: a = %d, b = %d", a, b);
+  printf("Après échange : a = %d, b = %d", a, b);
   return 0;
 }
 
