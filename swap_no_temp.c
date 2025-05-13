e1f8ecbf8f895b72c492df0b9df1eb5fcdcc5049
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-13 16:24:30 +0100

    Correction

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -5,12 +5,11 @@ int main()
     int a , b;
 
     scanf("%d %d", &a, &b);
-    printf("Avant échange: a = %d, b = %d\n", a, b);
 
     a = a + b;
     b = a - b;
     a = a - b;
 
-    printf("Après échange: a = %d, b = %d\n", a, b);
+    printf("a = %d, b = %d\n", a, b);
     return 0;
 }
