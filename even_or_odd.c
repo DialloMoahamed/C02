b27439ac97da3f1e7b63d9d19badb8c1fd9f64cb
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-13 11:48:26 +0100

    Comit

--- even_or_odd.c
+++ even_or_odd.c
@@ -3,15 +3,16 @@
 int main(int argc, char const *argv[])
 {
     int a;
+
     scanf("%d", &a);
-    if (a % 2 != 0)
-    {
-        printf("%d est un nombre impair.", a);
-    }
-    else
+
+    if (a % 2 == 0)
     {
         printf("%d est un nombre pair.", a);
     }
-    
+    else if (a % 2 != 0)
+    {
+        printf("%d est un nombre impair.", a);
+    }
     return 0;
 }
