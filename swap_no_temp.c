40fd6d7cb6b20bff1a3772bced161cbbde71e2b3
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-13 17:52:58 +0100

    Dernier push

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -2,10 +2,11 @@
 
 int main()
 {
-    int a = 5 , b = 3;
+    int a ,  b;
+    scanf("%d %d", &a, &b);
     a = a + b;
     b = a - b;
     a = a - b;
-    printf("Après échange: a = %d, b = %d", a, b);
+    printf("Après échange: a = %d, b = %d\n", a, b);
     return 0;
 }
